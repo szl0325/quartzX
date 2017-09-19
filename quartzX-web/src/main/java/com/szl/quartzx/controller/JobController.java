e19b6a1afddb6a6cf9a32adbd468c01d8a8d64ab
sunzhanlong <sunzhanlong@jd.com>
2017-09-19 15:50:03 +0800

    添加 log4j2 日志管理

--- quartzX-web/src/main/java/com/szl/quartzx/controller/JobController.java
+++ quartzX-web/src/main/java/com/szl/quartzx/controller/JobController.java
@@ -1,15 +1,18 @@
 package com.szl.quartzx.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/job")
 public class JobController {
+    static Logger logger = LogManager.getLogger(JobController.class.getName());
 
     @RequestMapping("/showPage")
     public String showPage(){
-        System.out.println("xxx");
+        logger.error("测试log4j2");
         return "job/jobList";
     }
 
