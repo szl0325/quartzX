202d5063ec2dd5cd9e386234d12f84f600376501
sunzhanlong <sunzhanlong@jd.com>
2017-10-09 16:33:44 +0800

    job_add

--- quartzX-web/src/main/webapp/misc/quartzx_js/job/job_add.js
+++ quartzX-web/src/main/webapp/misc/quartzx_js/job/job_add.js
@@ -1,4 +1,7 @@
 $(function () {
+    $("#myModal").on("hidden.bs.modal", function() {
+        clearForm($('#jobAddForm'));
+    });
     $("#saveJob").click(function () {
         if(validform().form()){
             alert('验证通过');
@@ -32,3 +35,18 @@ function validform()
         }
     });
 }
+
+function clearForm(form) {
+    // input清空
+    $(':input', form).each(function () {
+        var type = this.type;
+        var tag = this.tagName.toLowerCase(); // normalize case
+        if (type == 'text' || type == 'password' || tag == 'textarea')
+            this.value = "";
+        // 多选checkboxes清空
+        // select 下拉框清空
+        else if (tag == 'select')
+            this.selectedIndex = -1;
+        $(this).tooltip('destroy').removeClass(errorClass);
+    });
+};
